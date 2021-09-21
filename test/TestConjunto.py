ebbb24d06816a84aa1fd043cc1384fda582e2cac
keysiwin <77917170@continental.edu.pe>
2021-09-21 11:45:22 -0500

    Calcular promedio conjunto vacio (verde)

--- test/TestConjunto.py
+++ test/TestConjunto.py
@@ -3,5 +3,5 @@ from src.Conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
    def test_conjunto_vacio( self ):
-      conjunto = Conjunto()
+      conjunto = Conjunto([])
       self.assertIsNone(conjunto.promedio())
